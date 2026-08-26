632099bfaa1e82148e3393cb601c08bf23eff626
SJVDC <samuelvelandiad@gmail.com>
2026-08-26 14:10:58 -0500

    Feat: estrella dibujada

--- Taller1/Star.cpp
+++ Taller1/Star.cpp
@@ -1,12 +1,31 @@
 #include "Star.h"
+#include <GL/gl.h>
 
 //---------------------------------------------------------------------------------------------------
-Star :: Star( float radio ) : Object() {
-    Triangle* t1 = new Triangle( radio, 3 );
-    Triangle* t2 = new Triangle ( radio, 3 );
+Star :: Star ( float radio ) : Object(), radio( radio ) {
+    Triangle* t1 = new Triangle( 1.0f, 3 );
+    Triangle* t2 = new Triangle( 1.0f, 3 );
 
     this->AddChild( t1 );
     this->AddChild( t2 );
-};
+}
 //---------------------------------------------------------------------------------------------------
 Star :: ~Star() {};
+//---------------------------------------------------------------------------------------------------
+void Star :: Draw() {
+    glPushMatrix();
+        glScalef( this->radio, this->radio, 1.0f );
+        glColor3fv( this->Color );
+        if ( this->Children.size() > 0 && this->Children[ 0 ] != nullptr ) {
+            this->Children[ 0 ]->Draw();
+        }
+
+        if ( this-> Children.size() > 1 && this->Children[ 1 ] != nullptr ) {
+            glPushMatrix();
+                glRotatef( 180.0f, 0.0f, 0.0f, 1.0f );
+                this->Children[ 1 ]->Draw();
+            glPopMatrix();
+        }
+
+        glPopMatrix();
+}
--- Taller1/Star.h
+++ Taller1/Star.h
@@ -3,7 +3,10 @@
 #include "Triangle.h"
 
 class Star : public Object {
+    protected:
+        float radio { 1.0f };
     public:
         Star( float radio = 1.0f );
         virtual ~Star();
+        virtual void Draw() override;
 };
